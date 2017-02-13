5309a31efb9d69151bbbca506d910e622de04c89
Dolores Portalatin <admin@doloresportalatin.info>
2017-02-13 03:03:55 -0500

    fix bug with python dictionaries having a random order

--- process.py
+++ process.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-import os, datetime, json
+import os, datetime, json, collections
 from jinja2 import Environment, FileSystemLoader, Template
 
 
 filepath = os.path.dirname(os.path.abspath(__file__))
 
 with open('papers.json') as data:
-    d = json.load(data)
+    d = json.load(data, object_pairs_hook=collections.OrderedDict)
+    #d = json.load(data)
     html = ""
     for item in d["Headers"]:
         header = item
